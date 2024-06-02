8beee9ed84a2df2b52efc84231017284c48f9ba3
Halthomas65 <thaihao.dp@gmail.com>
2024-06-02 16:54:07 +0700

    Khởi động một dự án mới

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score = 0;
+    private float remainingTime;
+
+    public TextMeshProUGUI scoreText;
+
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI gameOverText;
+    public GameObject character;
+    public GameObject gemSpawner;
+
+
+    void Start() // đếm giờ khi trò chơi bắt đầu
+    {
+        remainingTime = 30f; //thời gian còn lại tại thời điểm bắt đầu bằng 30s (thời lượng của trò chơi)
+        StartCoroutine(CountdownTimer());
+        // là một phương thức nâng cao để gọi hàm CountdownTimer
+        // nhằm cho phép đồng hồ chạy song song, tiếp tục đếm khi chuyển qua frame mới và kết thúc ở frame mới khi đạt đúng thời gian
+    }
+
+    void Update()
+    {
+        if (remainingTime <= 0)
+        {
+            GameOver();
+        }
+        scoreText.text = "Score: " + score + " | Time: " + Mathf.CeilToInt(remainingTime); //Mathf.CeilToInt(remainingTime) làm tròn số nguyên dương
+    }
+
+    public static void AddScore(int amount)
+    {
+        score += amount;
+    }
+
+    private IEnumerator CountdownTimer()
+    {
+        while (remainingTime > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            remainingTime--;
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOverText.text = "Game Over!\nScore: " + score;
+        gameOverPanel.SetActive(true);
+        character.SetActive(false);
+        gemSpawner.SetActive(false);
+    }
+}
