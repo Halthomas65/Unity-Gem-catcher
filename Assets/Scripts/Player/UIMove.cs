fd47e138b6becaec31463f6de3fe72fd070d4752
Halthomas65 <thaihao.dp@gmail.com>
2024-11-01 22:13:28 +0700

    update car prefab

---
+++ Assets/Scripts/Player/UIMove.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    bool moveLeft;
+    bool moveRight;
+    float horizontalMove;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        moveLeft = false;
+        moveRight = false;
+    }
+
+    public void PointerDownLeft()
+    {
+        moveLeft = true;
+    }
+    
+    public void PointerUpLeft()
+    {
+        moveLeft = false;
+    }
+
+    
+    public void PointerDownRight()
+    {
+        moveRight = true;
+    }
+
+    
+    public void PointerUpRight()
+    {
+        moveRight = false;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (moveLeft)
+        {
+            horizontalMove = -speed;
+        }
+        else if (moveRight)
+        {
+            horizontalMove = speed;
+        }
+        else{
+            horizontalMove = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
+    }
+}
